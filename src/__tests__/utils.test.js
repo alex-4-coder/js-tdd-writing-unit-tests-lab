0108edcd24fee1743970fd41ccae8ba967ae96cc
alex kamau <alex.kamau1@student.moringaschool.com>
2025-07-19 05:28:41 -0700

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,26 @@
 // Your tests here
+const {isPalindrome} = require("../utils");
+
+describe("isPalindrome", () => {
+    it(" should return true for a single palindrome like racecar", () => {
+        expect(isPalindrome("racecar")).toBe(true)
+    });
+        it("should return false for a non-palindrome like 'car'", () => {
+             expect(isPalindrome("car")).toBe(false);
+    });
+         it("should return true for a mixed-case palindrome like 'racecar'", () => { 
+            expect(isPalindrome("racecar")).toBe(true);
+    });
+         it("should return false for an empty string", () => {
+            expect(isPalindrome("")).toBe(false);
+        
+    });
+         it("should throw an error for non-alphabetic characters", () => {
+           expect(() => isPalindrome("racecar123")).toThrow("Only alphabetic characters are allowed");
+  });
+         it("should throw an error if input is not a string", () => {
+             expect(() => isPalindrome(123)).toThrow("Input must be a string");
+  });
+
+    
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,21 @@
 // Your code here
+function isPalindrome(word) {
+  if (typeof word !== "string") {
+    throw new Error("Input must be a string");
+  }
+
+  if (word.length === 0) return false;
+
+  if (!/^[A-Za-z]+$/.test(word)) {
+    throw new Error("Only alphabetic characters are allowed");
+  }
+
+
+  const normalized = word.toLowerCase();
+  const reversed = normalized.split("").reverse().join("");
+  return normalized === reversed;
+}
+
+module.exports = {
+  isPalindrome,
+};
